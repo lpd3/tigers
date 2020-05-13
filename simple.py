b38a60e6db11a44c28efd0cfa183d37282184f5f
lpd3 <42684114+lpd3@users.noreply.github.com>
2020-05-12 20:20:23 -0400

    Delete simple.py

--- simple.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-from random import randint
-
-seed = 1526
-print('Content-Type: text/plain')
-print()
-# Smallest 5-digit multiple of 1526 is 1526*7
-# Largest 8-digit multiple of 1526 is 1526*65530
-print('Random value is {}'.format(seed*randint(7, 65530)))
